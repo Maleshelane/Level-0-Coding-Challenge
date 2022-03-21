9f79fc60197a61c93368906dac74f6fa61535054
Maleshelane <maleshelane28@gmail.com>
2022-03-21 22:54:11 +0200

    Second Commit

---
+++ task 2.js
@@ -0,0 +1,18 @@
+// x = 1 + 1 * 2
+// y = (1 + 1) * 2
+// z = 1 + ( 1 * 2 )
+// a = 1 + 1 * 2 / 2
+// b = (1 + 1 * 2 ) /  2
+
+// print all the things
+
+var x;
+x = 3;
+var y;
+y = 4;
+var z;
+z = x;
+var a;
+a = 2;
+var b;
+b = 1.5;
