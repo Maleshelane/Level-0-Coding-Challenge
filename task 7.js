e8a77fbab06fc11ee2191a33c7e6cf2e2b3bbe6c
Maleshelane <maleshelane28@gmail.com>
2022-03-21 23:07:57 +0200

    Seventh commit

---
+++ task 7.js
@@ -0,0 +1,13 @@
+function celsiusToFahrenheit (celsius) {
+    var cTemp = celsius;
+    var celsiusToF = (cTemp * 9/5) + 32;
+    return fahrenheit;
+}
+
+function fahrenheitToCelsius (fahrenheit){
+    var fTemp = fahrenheit;
+    varfahrenheitToC = (fTemp - 32 ) * 5/9;
+    return celsius;
+}
+celsiusToFahrenheit(20);
+fahrenheitToCelsius(40);
