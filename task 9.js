5948a5a5f4e5a1f9982b3df3cb458afc27a023c7
Maleshelane <maleshelane28@gmail.com>
2022-03-21 23:10:05 +0200

    Ninth commit

---
+++ task 9.js
@@ -0,0 +1,9 @@
+function vowelsFromString (word){
+    var vowel = [a,e,i,o,u,A,E,I,O,U ];
+    for (i = 0; i < word; i++);
+    if (vowel.includes (word[i]))
+
+    return vowel;
+
+}
+console.log(vowelsFromString(Maleshelane));
