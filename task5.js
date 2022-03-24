80c3ce45540386f09cd238bc4c99e2b6b508ef59
Maleshelane <91606084+Maleshelane@users.noreply.github.com>
2022-03-24 04:52:16 +0200

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,5 +1,5 @@
 function areaOfTriangle (){
-    const s = (side1 + side2+ side3)/2;
+    const sides = (side1 + side2+ side3)/2;
     const area = Math.sqrt (s* ((s- side1) * (s - side2) * (s - side3)));
 
     return areaValue;
