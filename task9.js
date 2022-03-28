b9fe37ce5288aecd8bc63cd550e43bdf40d479fa
Maleshelane <91606084+Maleshelane@users.noreply.github.com>
2022-03-28 05:37:03 +0200

    Update and rename task 9.js to task9.js

---
+++ task9.js
@@ -0,0 +1,9 @@
+function vowelsFromString (Maleshelane){
+    var vowel = [a,e,i,o,u,A,E,I,O,U ];
+    for (i = 0; i < Maleshelane; i++);
+    if (vowel.includes (Maleshelane[i]))
+
+    return vowel;
+
+}
+console.log(vowelsFromString(Maleshelane));
