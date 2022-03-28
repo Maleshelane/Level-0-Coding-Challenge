e5b122c02f441bd12206c29dea740d72e4e0c523
Maleshelane <91606084+Maleshelane@users.noreply.github.com>
2022-03-28 06:20:45 +0200

    Update task 8.js

--- task 8.js
+++ task 8.js
@@ -1,6 +1,7 @@
 function timeConvert (num){
     var hours = Math.floor (num/60);
     var minutes = num % 60;
-    return hours + minutes;
+    return hours + ":" + minutes;
 }
 console.log(timeConvert(133));
+console.log (timeConvert(71));
